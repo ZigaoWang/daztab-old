e790791d9fb4bca284bd1004fc1ac85e0f5cdfad
ZigaoWang <102006756+ZigaoWang@users.noreply.github.com>
2023-04-18 20:59:04 +0800

    Create time.js

---
+++ time.js
@@ -0,0 +1,8 @@
+`use strict`;
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000); 
