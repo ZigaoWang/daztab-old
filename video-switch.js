8e9a55ec82fa7f4257b521502de8e8b10a891dee
Zigao Wang <zigao.wang24@gmail.com>
2023-05-26 20:50:38 +0800

    Add files via upload

---
+++ video-switch.js
@@ -0,0 +1,15 @@
+var toggleSwitch = document.getElementById("toggleSwitch");
+var backgroundVideo = document.getElementById("backgroundVideo");
+
+// 默认显示图片
+backgroundVideo.style.display = "none";
+
+toggleSwitch.addEventListener("change", function() {
+  if (toggleSwitch.checked) {
+    // 开关打开，显示视频
+    backgroundVideo.style.display = "block";
+  } else {
+    // 开关关闭，隐藏视频
+    backgroundVideo.style.display = "none";
+  }
+});
