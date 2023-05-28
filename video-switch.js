75cfe447f44cd66b5648bf7714779fe32ddd9306
Zigao Wang <zigao.wang24@gmail.com>
2023-05-29 07:33:39 +0800

    Update video-switch.js

--- video-switch.js
+++ video-switch.js
@@ -8,6 +8,7 @@ toggleSwitch.addEventListener("change", function() {
   if (toggleSwitch.checked) {
     // 开关打开，显示视频
     backgroundVideo.style.display = "block";
+    toggleSwitch.parentNode.style.backgroundColor = "green";
   } else {
     // 开关关闭，隐藏视频
     backgroundVideo.style.display = "none";
