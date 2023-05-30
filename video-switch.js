805c8bc044096ee9dcffd3fd270fa1d10ef9c83b
Zigao Wang <zigao.wang24@gmail.com>
2023-05-30 21:46:12 +0800

    Update video-switch.js

--- video-switch.js
+++ video-switch.js
@@ -1,16 +1,30 @@
 var toggleSwitch = document.getElementById("toggleSwitch");
 var backgroundVideo = document.getElementById("backgroundVideo");
 
-// 默认显示图片
-backgroundVideo.style.display = "none";
+// 检查localStorage中是否存在用户的选择
+var userChoice = localStorage.getItem("userChoice");
+if (userChoice === "video") {
+  // 用户选择了视频
+  toggleSwitch.checked = true;
+  backgroundVideo.style.display = "block";
+  toggleSwitch.parentNode.style.backgroundColor = "green";
+} else {
+  // 用户选择了图片（默认）
+  toggleSwitch.checked = false;
+  backgroundVideo.style.display = "none";
+}
 
 toggleSwitch.addEventListener("change", function() {
   if (toggleSwitch.checked) {
-    // 开关打开，显示视频
+    // 用户选择了视频
     backgroundVideo.style.display = "block";
     toggleSwitch.parentNode.style.backgroundColor = "green";
+    // 存储用户的选择
+    localStorage.setItem("userChoice", "video");
   } else {
-    // 开关关闭，隐藏视频
+    // 用户选择了图片
     backgroundVideo.style.display = "none";
+    // 清除用户的选择
+    localStorage.removeItem("userChoice");
   }
 });
