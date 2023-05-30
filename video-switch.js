63ca1902c2bda203ede6bdbff19f775b041766f9
Zigao Wang <zigao.wang24@gmail.com>
2023-05-31 07:41:50 +0800

    Update video-switch.js

--- video-switch.js
+++ video-switch.js
@@ -8,6 +8,7 @@ if (userChoice === "video") {
   toggleSwitch.checked = true;
   backgroundVideo.style.display = "block";
   toggleSwitch.parentNode.style.backgroundColor = "green";
+  loadVideo();
 } else {
   // 用户选择了图片（默认）
   toggleSwitch.checked = false;
@@ -21,10 +22,33 @@ toggleSwitch.addEventListener("change", function() {
     toggleSwitch.parentNode.style.backgroundColor = "green";
     // 存储用户的选择
     localStorage.setItem("userChoice", "video");
+    loadVideo();
   } else {
     // 用户选择了图片
     backgroundVideo.style.display = "none";
     // 清除用户的选择
     localStorage.removeItem("userChoice");
   }
+});
+
+function loadVideo() {
+  // 检查localStorage中是否已经缓存了视频
+  var videoCached = localStorage.getItem("videoCached");
+  if (videoCached === "true") {
+    // 从缓存加载视频
+    backgroundVideo.play();
+  } else {
+    // 首次加载视频，进行缓存
+    backgroundVideo.addEventListener("canplaythrough", function() {
+      // 视频加载完成后显示并播放
+      backgroundVideo.style.display = "block";
+      backgroundVideo.play();
+      // 标记视频已缓存
+      localStorage.setItem("videoCached", "true");
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+  loadVideo();
 });
